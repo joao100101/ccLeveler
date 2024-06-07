b8923929bc2048674355d23e60ba83e87c3b77fb
joao100101 <joaovitor20002@gmail.com>
2024-06-06 22:44:26 -0300

    Atualizado

---
+++ levelerNew.lua
@@ -0,0 +1,60 @@
+-- Função para solicitar valores do usuário
+local function prompt(message)
+    print(message)
+    return tonumber(read())
+end
+
+-- Solicitar coordenadas do usuário
+local xMin = prompt("Digite o valor de X mínimo:")
+local xMax = prompt("Digite o valor de X máximo:")
+local zMin = prompt("Digite o valor de Z mínimo:")
+local zMax = prompt("Digite o valor de Z máximo:")
+local x, y, z = gps.locate()
+
+-- Inicializar a turtle
+local function initializeTurtle()
+    print("Iniciando a turtle...")
+    turtle.select(1)
+end
+
+-- Função para verificar e colocar bloco abaixo da turtle
+local function placeBlockBelow()
+    if not turtle.detectDown() then
+        turtle.placeDown()
+    end
+end
+
+local function updateLocation()
+    x, y, z = gps.locate()
+end
+
+local function changeLine()
+    turtle.turnRight()
+    if not turtle.forward() then
+        turtle.dig()
+    end
+    turtle.forward()
+    turtle.turnRight()
+    if z >= zMin and z <= zMax then
+        walk()
+    end
+end
+
+local function walk()
+    while x < xMax and x > xMin and z > zMin and z < zMax do
+        if turtle.getFuelLevel() > 0 then
+            if not turtle.forward() then
+                turtle.dig()
+            end
+            placeBlockBelow()
+            turtle.forward()
+            updateLocation()
+        else
+            print("Falha, falta de combustivel.")
+            break
+        end
+    end
+    changeLine()
+end
+
+walk()
